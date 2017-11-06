ed94e22cf7107159a225cff6ac89fc024b9ff521
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-11-06 02:51:27 -0200

    Improved code structure

--- index.js
+++ index.js
@@ -2,32 +2,16 @@ const zombies = require('./zombies.json');
 const express = require('express');
 
 zombies.forEach(zombie => {
-    let app = express();
+    const app = express();
     zombie.routes.forEach(route => {
-
-        let handler = (request, response) => {
+        const handler = (request, response) => {
             response.set(route.response.header)
                     .status(route.response.status)
                     .json(route.response.body);
         };
 
-        switch (route.method) {
-            case 'GET':
-                app.get(route.path, handler);
-                break;
-            case 'POST':
-                app.post(route.path, handler);
-                break;
-            case 'PUT':
-                app.put(route.path, handler);
-                break;
-            case 'DELETE':
-                app.delete(route.path, handler);
-                break;
-            case 'OPTIONS':
-                app.options(route.path, handler);
-                break;
-        }
+        const method = route.method.toLowerCase();
+        app[method](route.path, handler);
     });
     app.listen(zombie.port, console.log('Zombie ' + zombie.port + ' is walking'));
 });
